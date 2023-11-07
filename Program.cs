24f4f7849c266df22601b6b13f258cd31feb1bb5
Vitor Alves <vitor.dev.alves@gmail.com>
2023-11-07 19:41:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿string welcomeMessage = @"𝓐𝓴𝓲𝓵𝓪❜𝓼 𝓑𝓲𝓼𝓽𝓻𝓸";
+
+Dictionary<string, List<int>> dishList = new Dictionary<string, List<int>>();
+dishList.Add("File a Parmegiana", new List<int> {10, 8, 9, 6});
+dishList.Add("Strogonoff de frango", new List<int> {3,4,10});
+
+void ShowLogo()
+{
+    Console.WriteLine(welcomeMessage);
+}
+
+void ShowMenuOptions()
+{
+    ShowLogo();
+    Console.WriteLine("\nMenu:\n");
+    Console.WriteLine("1- Registrar um novo prato");
+    Console.WriteLine("2- Mostrar todos os pratos");
+    Console.WriteLine("3- Avaliar um prato");
+    Console.WriteLine("4- Consultar a avaliação de um prato");
+    Console.WriteLine("0- Sair");
+    Console.Write("\nDigite a opção de sua escolha: ");
+
+    string selectedOption = Console.ReadLine()!;
+
+    int selectedOptionNumber = int.Parse(selectedOption);
+
+    switch (selectedOptionNumber)
+    {
+        case 1: PostDish();
+            break;
+        case 2: ShowDishs();
+            break;
+        case 3: RateDishs();
+            break;
+        case 4: ShowDishRate();
+            break;
+        case 0: Console.WriteLine("Foi um prazer ter você por aqui, volte sempre!");
+            break;
+        default: Console.WriteLine("Opção inválida!");
+            break;
+    }
+}
+
+void showOptionTitle(string tittle)
+{
+    int letterAccount = tittle.Length;
+    string asterisks = string.Empty.PadLeft(letterAccount, '*');
+    Console.WriteLine(asterisks);
+    Console.WriteLine(tittle);
+    Console.WriteLine(asterisks + "\n");
+}
+
+void PostDish(){
+    Console.Clear();
+    ShowLogo();
+    showOptionTitle("Registrar um novo prato");
+    Console.WriteLine("Digite o nome do seu novo prato: ");
+    string dishName = Console.ReadLine()!;
+    dishList.Add(dishName, new List<int>());
+    Console.WriteLine($"O prato {dishName} foi registrado com sucesso!");
+    Thread.Sleep(3000);
+    Console.Clear();
+    ShowMenuOptions();
+}
+
+void ShowDishs()
+{
+    Console.Clear();
+    showOptionTitle("Exibindo todos os pratos");
+    foreach (string dish in dishList.Keys)
+    {
+        Console.WriteLine($"{dish}");
+    }
+    Console.WriteLine("\nAperte qualquer tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Console.Clear();
+    ShowMenuOptions();
+}
+
+void RateDishs()
+{
+    Console.Clear();
+    showOptionTitle("Avaliar prato");
+    Console.Write("Escolha o prato que deseja avaliar:");
+    string dishNameforRate = Console.ReadLine()!;
+    if (dishList.ContainsKey(dishNameforRate))
+    {
+        Console.Write($"O prato {dishNameforRate} merece a nota: ");
+        int note = int.Parse(Console.ReadLine()!);
+        dishList[dishNameforRate].Add(note);
+        Console.WriteLine($"\nA nota {note} para {dishNameforRate} foi registrada com sucesso!");
+        Thread.Sleep(3000);
+        Console.Clear();
+        ShowMenuOptions();
+    } else
+    {
+        Console.WriteLine($"\nO prato {dishNameforRate} não faz parte do cardápio.");
+        Console.WriteLine("\nAperte qualquer tecla para voltar ao menu principal.");
+        Console.ReadKey();
+        Console.Clear();
+        ShowMenuOptions();
+    }
+}
+
+void ShowDishRate()
+{
+    Console.Clear();
+    showOptionTitle("Consultar avaliação do prato");
+    Console.Write("Escolha o prato que deseja ver a avaliação:");
+    string dishNameRated = Console.ReadLine()!;
+    if (dishList.ContainsKey(dishNameRated))
+    {
+        List<int> values = dishList[dishNameRated];
+        if (values != null && values.Any())
+        {
+            double media = values.Average();
+            string mediaWithTwoCases = media.ToString("0.00");
+            Console.WriteLine($"\nO prato {dishNameRated} tem a seguinte média {mediaWithTwoCases}.\n");
+            Console.WriteLine("\nAperte qualquer tecla para voltar ao menu principal.");
+            Console.ReadKey();
+            Console.Clear();
+            ShowMenuOptions();
+        }else
+        {
+            Console.WriteLine($"\nO prato {dishNameRated} ainda não possúi avaliações.\n");
+            Console.WriteLine("\nAperte qualquer tecla para voltar ao menu principal.");
+            Console.ReadKey();
+            Console.Clear();
+            ShowMenuOptions();
+        }
+    } else
+    {
+        Console.WriteLine($"\nO prato {dishNameRated} não faz parte do cardápio.");
+        Console.WriteLine("\nAperte qualquer tecla para voltar ao menu principal.");
+        Console.ReadKey();
+        Console.Clear();
+        ShowMenuOptions();
+    }
+}
+
+ShowMenuOptions();
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Avaliação de Pratos do Akila's Bistro
+
+Bem-vindo ao **Avaliação de Pratos do Akila's Bistro**, uma aplicação simples em C# que permite a você registrar pratos, avaliá-los e consultar suas avaliações.
+
+## Sobre o Projeto
+
+Esta aplicação foi desenvolvida em C# como parte de um exercício para demonstrar o uso de dicionários e estruturas de controle básicas. Ela permite que você faça o seguinte:
+
+- Registrar novos pratos no cardápio.
+- Avaliar pratos existentes, dando-lhes uma nota.
+- Consultar a média das avaliações para um prato específico.
+- Visualizar a lista de todos os pratos disponíveis.
+
+## Como Usar
+
+1. Execute o programa, e você verá um menu de opções.
+2. Escolha uma das opções digitando o número correspondente.
+3. Siga as instruções na tela para realizar a ação desejada.
+
+## Funcionalidades
+
+- **Registrar um Novo Prato**: Você pode adicionar novos pratos ao cardápio digitando o nome do prato. A aplicação irá registrá-lo para você.
+
+- **Mostrar Todos os Pratos**: Visualize todos os pratos disponíveis no cardápio.
+
+- **Avaliar um Prato**: Avalie um prato existente, atribuindo-lhe uma nota. A nota será registrada para o prato.
+
+- **Consultar Avaliação de um Prato**: Veja a média das avaliações para um prato específico no cardápio.
+
+- **Sair**: Encerre a aplicação.
+
+## Requisitos
+
+- Você deve ter o ambiente de desenvolvimento C# configurado.
+
+## Autor
+
+- Vitor Alves
+
